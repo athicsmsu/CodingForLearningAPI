c74ff30d2a809c44e4c3a847345f90c33d2294a6
athicsmsu <65011212145@msu.ac.th>
2025-05-01 01:02:52 +0700

    user

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 import { router as index } from "./api/index";
-// import { router as user } from "./api/user";
+import { router as user } from "./api/user";
 import { router as code } from "./api/CodeCompile";
 
 
@@ -20,5 +20,5 @@ app.use(bodyParser.text());
 app.use(bodyParser.json());
 
 app.use("/", index);
-// app.use("/user", user);
+app.use("/user", user);
 app.use("/code", code);
