c1763c78f87bbc71def5b267dc595079207481be
BobbyCsMsu <65011212103@msu.ac.th>
2025-09-05 16:55:22 +0700

    new daatabase

--- dbconnect.ts
+++ dbconnect.ts
@@ -3,9 +3,9 @@ import util from "util";
 
 export const conn = mysql.createPool({
   connectionLimit: 10,
-  host: "202.28.34.197",
-  user: "cp_65011212145",
-  password: "65011212145@csmsu",
-  database: "cp_65011212145",
+  host: "phpmyadmin.alwaysdata.com",
+  user: "athijd",
+  password: "Aa11bb22.",
+  database: "athijd_historyplay",
 });
 export const queryAsync = util.promisify(conn.query).bind(conn);
